0486b0863e555f175d053aa4947b4fde702cd0de
Pedro Carmezim <pedrocarmezim00@gmail.com>
2018-12-29 02:39:41 +0000

    add simplegraphics and build config, jar build success

--- src/main/java/org/pcdev/simplegames/App.java
+++ src/main/java/org/pcdev/simplegames/App.java
@@ -1,10 +1,14 @@
 package org.pcdev.simplegames;
 
+import org.academiadecodigo.simplegraphics.graphics.Rectangle;
+
 public class App {
     public static void main(String[] args) {
 
-        System.out.println("Let PLay");
+        System.out.println("Lets PLay");
 
+        Rectangle rec = new Rectangle(10,10,200,200);
+        rec.draw();
 
     }
 }
